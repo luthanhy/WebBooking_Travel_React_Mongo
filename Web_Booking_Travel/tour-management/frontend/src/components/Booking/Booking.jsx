c7930ca214264c7c0421db0c7ea723c51259a5bd
noname77777777777777777 <technologyengineer7@gmail.com>
2024-05-10 10:37:16 +0700

    Booking complete

--- Web_Booking_Travel/tour-management/frontend/src/components/Booking/Booking.jsx
+++ Web_Booking_Travel/tour-management/frontend/src/components/Booking/Booking.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Booking/booking.css'
 import { ListGroup,ListGroupItem,FormGroup,Button,Form, Input} from 'reactstrap'
+import { Navigate } from 'react-router-dom'
 
 const Booking = ({tour,AgvRating}) => {
   const {price,reviews} = tour
+  const [credentials,setCredentials] = useState({
+    userId:'01',
+    userEmail:'example@gmail.com',
+    fullName:'',
+    phone:'',
+    getSize:1,
+    bookAt:''
+  })
+  const ServiceFee = 10;
+  const TotalAmount = Number(price) * Number(credentials.getSize) + ServiceFee;
   const handleChange = e =>{
-
+    setCredentials(prev=> ({...prev,[e.target.id]:e.target.value}))
   }
+  const handleClick = e =>{
+    e.preventDefault();
+    console.log(credentials)
+}
   return (
     <>
         <div className='booking'>
@@ -17,9 +32,10 @@ const Booking = ({tour,AgvRating}) => {
                     <i className=' ri-star-fill'></i>
                 </span>
             </div>
-            <h3>Information</h3>
+            <h3 style={{paddingTop:'1.5rem',paddingBottom:'1rem'}}>Information</h3>
         <div className='booking_form'>
-            <Form className = 'booking_info_form'>
+            <Form className = 'booking_info_form' onSubmit={handleClick}>
+                <div className='booking_info1'>
                 <FormGroup >
                     <input type="text" placeholder='Full Name' id='full_name'required onChange={handleChange}/>
                 </FormGroup>
@@ -29,11 +45,20 @@ const Booking = ({tour,AgvRating}) => {
                 <FormGroup >
                     <input type="text" placeholder='Full Na me' id='book_at' required onChange={handleChange}/>
                 </FormGroup>
+                </div>
                 <FormGroup>
                     <input type="date" placeholder='' id='bookAt' required onChange={handleChange}/>
-                    <input type="number" placeholder='' id='getSize' required onChange={handleChange}/>
+                    <input type="number" placeholder='Guest' id='getSize' required onChange={handleChange}/>
                 </FormGroup>
             </Form>
+        </div>
+        <div className="booking_bottom">
+            <ListGroup>
+                <ListGroupItem className=" border-0 px-0 d"><h5 className=' d-flex align-items-center gap-1'>${price}<i className=' ri-close-line'>1 person</i></h5> <span>${price}</span></ListGroupItem>
+                <ListGroupItem className=" border-0 px-0"><h5>Service Charge </h5> <span>${ServiceFee}</span></ListGroupItem>
+                <ListGroupItem className="total_Y border-0 px-0"><h5>Total</h5> <span>${TotalAmount}</span></ListGroupItem>
+            </ListGroup>
+            <Button className=' btn primary__btn w-100 mt-4' onClick={handleClick}>Book Now</Button>
         </div>
         </div>
     </>
