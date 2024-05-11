ecd889ef7f7a43530bb575ab55c42007b1966109
noname77777777777777777 <technologyengineer7@gmail.com>
2024-05-11 11:41:03 +0700

    connect mongo db succes

--- Web_Booking_Travel/tour-management/backend/index.js
+++ Web_Booking_Travel/tour-management/backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 dotenv.config()
@@ -11,8 +12,13 @@ const port = process.env.PORT || 8000;
 
 const connect = async() =>{
     try {
-        
+        await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true
+        }) 
+        console.log("Connect Success");
     } catch (error) {
+        console.log("Connect Mongo DB failed");
         console.log(error);
     }
 }
@@ -26,6 +32,7 @@ app.use(cors());
 app.use(cookieParser());
 
 app.listen(port,()=>{
+    connect();
     console.log('server listen port : ',port)
 })
 
