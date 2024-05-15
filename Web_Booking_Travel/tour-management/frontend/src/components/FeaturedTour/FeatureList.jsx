0ef57d66a2a237da367141405625cdd7bae1c4a0
noname77777777777777777 <technologyengineer7@gmail.com>
2024-05-15 10:47:19 +0700

    set callback backend

--- Web_Booking_Travel/tour-management/frontend/src/components/FeaturedTour/FeatureList.jsx
+++ Web_Booking_Travel/tour-management/frontend/src/components/FeaturedTour/FeatureList.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import TourCard from '../../shared/TourCard'
 import TourData from '../../assets/data/tours'
 import { Col } from 'reactstrap'
-
+import useFetch from '../../hooks/useFetch'
+import { BASE_URL } from '../../utils/config'
 const FeatureList = () => {
+  // const {data:featuredTour} = useFetch(&{BASE_URL}\search)
   return <>
-  
+    
 {
     TourData?.map(tour=>(
         <Col lg="3" className="mb-4" key={tour.id}>
