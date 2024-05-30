490f617933a85d97e60d679f54e19bd59f6b367d
noname77777777777777777 <technologyengineer7@gmail.com>
2024-05-31 06:15:16 +0700

    init smtp

--- Web_Booking_Travel/tour-management/backend/routes/SMTPMail.js
+++ Web_Booking_Travel/tour-management/backend/routes/SMTPMail.js
@@ -1,1 +1,32 @@
 import NodeMailer from "nodemailer";
+import dotenv from "dotenv";
+ 
+dotenv.config();
+const emailAdmin  = process.env.USER_SMTP || "";
+const emailPass = process.env.PASS_SMTP || "";
+
+const emails = 'luthanhy1@gmail.com'
+
+async function main(){
+    let transporter = NodeMailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure:true,
+        auth:{
+            user: emailAdmin,
+            pass: emailPass,
+        }
+    });
+    let info  = transporter.sendMail({
+            from: "YOU",
+            to:emails,
+            subject:"TEST",
+            html: `<h1>TEST</h1><p>Bomaytest</p>`,
+    });
+    console.log(info.messageId);
+    console.log(info.accepted); // Array of emails that were successful
+    console.log(info.rejected); // Array of unsuccessful emails
+
+main()
+.catch(err => console.log(err));
+}
