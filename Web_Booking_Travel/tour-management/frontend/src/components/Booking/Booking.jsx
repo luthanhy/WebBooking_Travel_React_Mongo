256fc267808e4d230c188d96af5927e2bfb4b1bf
noname77777777777777777 <technologyengineer7@gmail.com>
2024-06-02 21:58:30 +0700

    fix booking

--- Web_Booking_Travel/tour-management/frontend/src/components/Booking/Booking.jsx
+++ Web_Booking_Travel/tour-management/frontend/src/components/Booking/Booking.jsx
@@ -6,6 +6,7 @@ import { BASE_URL } from '../../utils/config.js';
 import { useIsLoggedIn } from '../../utils/auth.js';
 
 const Booking = ({ tour, AgvRating }) => {
+  const [error, setError] = useState('') ;
   const userData = localStorage.getItem("user");
   let parsedUserData = null;
   if (userData) {
@@ -22,21 +23,18 @@ const Booking = ({ tour, AgvRating }) => {
     fullName: '',
     tourName: '',
     phoneNumber: '',
-    guestSize: 1, // Start as a positive number
+    guestSize: 1,
     BookAt: ''
   });
 
-  const  today = new Date().toISOString().split('T')[0];
+  const today = new Date().toISOString().split('T')[0];
   const maxDate = new Date();
   maxDate.setMonth(maxDate.getMonth() + 3);
   const maxDateString = maxDate.toISOString().split('T')[0];
 
-  console.log("Tour Title:", tour.title);
   credentials.tourName = tour.title;
-  console.log("isLoggedIn:", useIsLoggedIn);
-  
+
   if (useIsLoggedIn && parsedUserData) {
-    console.log("Parsed User Data:", parsedUserData);
     credentials.userId = parsedUserData._id;
   }
 
@@ -53,27 +51,24 @@ const Booking = ({ tour, AgvRating }) => {
   const handleClick = async (e) => {
     e.preventDefault();
 
-
     if (Number(credentials.phoneNumber) <= 0 || Number(credentials.guestSize) <= 0) {
-      alert("Phone number and guest size must be positive numbers.");
+      setError("Phone number and guest size must be positive numbers.");
       return;
     }
 
     const bookAtDate = new Date(credentials.BookAt);
 
-
     if (bookAtDate < new Date(today) || bookAtDate > new Date(maxDateString)) {
-      alert("The booking date must be within the next 3 months and within the current year.");
+      setError("The booking date must be within the next 3 months and within the current year.");
       return;
     }
 
     if (bookAtDate.getFullYear() !== new Date().getFullYear()) {
-      alert("The booking date must be within the current year.");
+      setError("The booking date must be within the current year.");
       return;
     }
 
     try {
-      console.log(`${BASE_URL}/booking/`);
       const res = await fetch(`${BASE_URL}/booking/`, {
         method: "POST",
         headers: {
@@ -81,56 +76,67 @@ const Booking = ({ tour, AgvRating }) => {
         },
         body: JSON.stringify(credentials),
       });
+
       const result = await res.json();
-      console.log("Response JSON:", result);
-      console.log("Data:", result.data);
+
+      if (res.ok) {
+        navigate("/choseMethodPayment", { state: { ...credentials, totalAmount: TotalAmount, tourName: tour.title } });
+      } else {
+        console.error("Error booking:", result);
+      }
     } catch (error) {
       console.error("Error occurred:", error);
     }
-    console.log(credentials);
-    navigate("/thank-you");
   };
 
   return (
-    <>
-      <div className='booking'>
-        <div className='booking_top d-flex align-items-center justify-content-between'>
-          <h3>{tour.price}$/per person</h3>
-          <span>
-            {AgvRating === 0 ? null : AgvRating} {(tour && Array.isArray(tour.reviews) ? tour.reviews.length : 0)}
-            <i className='ri-star-fill'></i>
-          </span>
-        </div>
-        <h3 style={{ paddingTop: '1.5rem', paddingBottom: '1rem' }}>Information</h3>
-        <div className='booking_form'>
-          <Form className='booking_info_form' onSubmit={handleClick}>
-            <div className='booking_info1'>
-              <FormGroup>
-                <input type="text" placeholder='Full Name' id='fullName' required onChange={handleChange} />
-              </FormGroup>
-              <FormGroup>
-                <input type="number" placeholder='Phone' id='phoneNumber' min="1" required onChange={handleChange} />
-              </FormGroup>
-              <FormGroup>
-                <input type="text" placeholder='Email' id='userEmail' required onChange={handleChange} />
-              </FormGroup>
-            </div>
-            <FormGroup>
-              <input type="date" placeholder='' id='BookAt' min={today} max={maxDateString} required onChange={handleChange} />
-              <input type="number" placeholder='Guest' id='guestSize' min="1" required onChange={handleChange} />
-            </FormGroup>
-          </Form>
-        </div>
-        <div className="booking_bottom">
-          <ListGroup>
-            <ListGroupItem className="border-0 px-0 d"><h5 className='d-flex align-items-center gap-1'>${tour.price}<i className='ri-close-line'>1 person</i></h5> <span>${tour.price}</span></ListGroupItem>
-            <ListGroupItem className="border-0 px-0"><h5>Service Charge</h5> <span>${ServiceFee}</span></ListGroupItem>
-            <ListGroupItem className="total_Y border-0 px-0"><h5>Total</h5> <span>${TotalAmount}</span></ListGroupItem>
-          </ListGroup>
-          <Button className='btn primary__btn w-100 mt-4' onClick={handleClick}>Book Now</Button>
-        </div>
+    <div className='booking'>
+      <div className='booking_top d-flex align-items-center justify-content-between'>
+        <h3>{tour.price}$/per person</h3>
+        <span>
+          {AgvRating === 0 ? null : AgvRating} {(tour && Array.isArray(tour.reviews) ? tour.reviews.length : 0)}
+          <i className='ri-star-fill'></i>
+        </span>
       </div>
-    </>
+      <h3 style={{ paddingTop: '1.5rem', paddingBottom: '1rem' }}>Information</h3>
+      {error && <div className="alert alert-danger">{error}</div>}
+      <div className='booking_form'>
+        <Form className='booking_info_form' onSubmit={handleClick}>
+          <div className='booking_info1'>
+            <FormGroup>
+              <input type="text" placeholder='Full Name' id='fullName' required onChange={handleChange} />
+            </FormGroup>
+            <FormGroup>
+              <input type="number" placeholder='Phone' id='phoneNumber' min="1" required onChange={handleChange} />
+            </FormGroup>
+            <FormGroup>
+              <input type="text" placeholder='Email' id='userEmail' required onChange={handleChange} />
+            </FormGroup>
+          </div>
+          <FormGroup>
+            <input type="date" id='BookAt' min={today} max={maxDateString} required onChange={handleChange} />
+            <input type="number" placeholder='Guest' id='guestSize' min="1" required onChange={handleChange} />
+          </FormGroup>
+        </Form>
+      </div>
+      <div className="booking_bottom">
+        <ListGroup>
+          <ListGroupItem className="border-0 px-0 d">
+            <h5 className='d-flex align-items-center gap-1'>${tour.price}<i className='ri-close-line'>1 person</i></h5>
+            <span>${tour.price}</span>
+          </ListGroupItem>
+          <ListGroupItem className="border-0 px-0">
+            <h5>Service Charge</h5>
+            <span>${ServiceFee}</span>
+          </ListGroupItem>
+          <ListGroupItem className="total_Y border-0 px-0">
+            <h5>Total</h5>
+            <span>${TotalAmount}</span>
+          </ListGroupItem>
+        </ListGroup>
+        <Button className='btn primary__btn w-100 mt-4' onClick={handleClick}>Book Now</Button>
+      </div>
+    </div>
   );
 }
 
