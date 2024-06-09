e626729441450bbf2dd0909760bb4feb48815d50
noname77777777777777777 <technologyengineer7@gmail.com>
2024-06-09 15:48:20 +0700

    setup build backend

--- Web_Booking_Travel/tour-management/backend/src/utils/SMTPMail.js
+++ Web_Booking_Travel/tour-management/backend/src/utils/SMTPMail.js
@@ -1,6 +1,6 @@
 import NodeMailer from 'nodemailer';
 
-export const SMTPSendMail = async (emailAdmin, emailPass, recipient ,Info) => {
+export const SMTPSendMail = async (emailAdmin, emailPass, Info) => {
     try {
         let transporter = NodeMailer.createTransport({
             host: "smtp.gmail.com",
@@ -14,9 +14,9 @@ export const SMTPSendMail = async (emailAdmin, emailPass, recipient ,Info) => {
 
         let info = await transporter.sendMail({
             from: emailAdmin,
-            to: recipient,
+            to: Info.userEmail,
             subject: "Subscription Confirmation",
-            html: `<h1>Thank you for subscribing!</h1><p>You have successfully subscribed to our travel newsletter.</p> <h1>Booking Information<h1> <p>Price : $ </p> <p>Book At : </p> <p>Amount : </p> <p>Tour Name : </p><p>Full Name : </p> <p> Phone Number : </p> <p>Transaction : </p> `,
+            html: `<h1>Thank you for subscribing!</h1><p>You have successfully subscribed to our travel newsletter.</p> <h1>Booking Information<h1> <p>Name : ${Info.fullName}</p> <p>Price : ${Info.fullName} </p> <p>Book At : </p> <p>Amount : </p> <p>Tour Name : </p><p>Full Name : </p> <p> Phone Number : </p> <p>Transaction : </p> `,
         });
 
         console.log("Message sent: %s", info.messageId);
